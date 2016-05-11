cf4d3b426261f8d836bf9aab19b0fa5b43b6242f
Tomilin Denis <dt0xff@gmail.com>
2016-05-11 22:42:22 +0300

    Added simple single mappers for tests

---
+++ tests/mappers.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, division
+from louis.mapper import Mapper
+
+
+class SimpleMapper(Mapper):
+    class Meta:
+        source = 'item'
+        many = False
+
+    external_id = '@id'
+    a = '@a'
+
+
+class SimpleProcessorMapper(Mapper):
+    class Meta:
+        source = 'item'
+        many = False
+
+    external_id = '@id', int
+    a = '@a', lambda v: v.split()
