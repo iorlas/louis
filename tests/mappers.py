28d4aff83553813f9a56d9c8cac8add32a9c0eb0
Denis Timilin <dt0xff@gmail.com>
2016-05-12 02:08:07 +0300

    Sync test mappers too

--- tests/mappers.py
+++ tests/mappers.py
@@ -1,45 +1,40 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, division
-from louis.mapper import Mapper
+from louis.mapper import Mapper, CollectionMapper
 
 
 class SimpleMapper(Mapper):
-    class Meta:
-        source = 'item'
-        many = False
+    source = 'item'
 
     external_id = '@id'
     a = '@a'
 
 
 class SimpleProcessorMapper(Mapper):
-    class Meta:
-        source = 'item'
-        many = False
+    source = 'item'
 
     external_id = '@id', int
     a = '@a', lambda v: v.split()
 
 
-class SimpleManyMapper(Mapper):
-    class Meta:
-        source = 'item'
-        many = True
+class SimpleManyMapper(CollectionMapper):
+    source = 'item'
 
     external_id = '@id', int
     a = '@a'
 
 
 class SimpleMapperWithExternalID(Mapper):
-    class Meta:
-        source = 'item'
-        many = False
-        external_id_field = 'external_id'
+    source = 'item'
+    external_id_field = 'external_id'
 
     external_id = '@id', int
     a = '@a'
 
 
-class SimpleManyMapperWithExternalID(SimpleMapperWithExternalID):
-    class Meta(SimpleMapperWithExternalID.Meta):
-        many = True
+class SimpleManyMapperWithExternalID(CollectionMapper):
+    source = 'item'
+    external_id_field = 'external_id'
+
+    external_id = '@id', int
+    a = '@a'
