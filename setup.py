226da0a8a59259a11fff86d22c11a437c1353377
Tomilin Denis <dt0xff@gmail.com>
2016-05-11 20:40:26 +0300

    Common structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='louis',
+    version='0.1',
+    description='Events data scrapping library',
+    author='Denis Tomilin',
+    author_email='dt0xff@gmail.com',
+    packages=['louis']
+)
