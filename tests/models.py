5c7762cc6881cda5ca3a33c49ead879d5a6c801c
Denis Timilin <dt0xff@gmail.com>
2016-05-12 02:58:09 +0300

    Missing tests models

---
+++ tests/models.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, division
+from django.db import models
+
+
+class SimpleModel(models.Model):
+    external_id = models.CharField(max_length=1024)
